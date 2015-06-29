11fb21bca4022f4a866fea6484864ed2e0f547cf
Weston Novelli <wan1@rice.edu>
2015-06-29 17:18:56 -0500

    made the results page a bit prettier

--- openbar/openbar_search/models.py
+++ openbar/openbar_search/models.py
@@ -77,6 +77,7 @@ class Query(models.Model):
     subject = models.TextField()
     title = models.TextField()      # This may change
     complexity_score = models.ForeignKey(ComplexityScore, null=True)
+    short_form = models.CharField(max_length=140, default="Preview of the page")
     objects = MainManager()
 
 
