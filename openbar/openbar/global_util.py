1b928a7e8e4ce86a660834d6568ec68b6c651af4
Weston Novelli <wan1@rice.edu>
2015-07-06 09:48:57 -0500

    fix for the complexity score conversion error

--- openbar/openbar/global_util.py
+++ openbar/openbar/global_util.py
@@ -25,14 +25,13 @@ def convert(depth_of_material_score, num_to_letter=True):
     :return: a character corresponding to the score.
     """
     constant = 39
-    score_map = {1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I', 10: 'J', 11: 'K', 12: 'L',
-                 13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R', 19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W',
-                 24: 'X', 25: 'Y', 26: 'Z', 27: '-'}
+    score_map = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
+                 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W',
+                 23: 'X', 24: 'Y', 25: 'Z', 26: '-'}
     count = 0
-    score = 0
-    print depth_of_material_score
+    score = constant
     if num_to_letter:
-        while score < depth_of_material_score:
+        while score <= depth_of_material_score:
             count += 1
             score += constant
         return score_map[count]
