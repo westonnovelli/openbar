e4f4fb000c305a13fc983aaf249cd15d5e113e4e
Weston Novelli <wan1@rice.edu>
2015-07-08 11:28:24 -0500

    broke out complexity differential

--- openbar/openbar/global_util.py
+++ openbar/openbar/global_util.py
@@ -5,6 +5,8 @@ from django.db import models
 import inspect
 from enum import Enum
 
+def complexity_constant():
+    return 39
 
 class MainManager(models.Manager):
     """Helper to add new user to their respective group"""
@@ -18,13 +20,13 @@ class MainManager(models.Manager):
 def convert(depth_of_material_score, num_to_letter=True):
     """
     Converts the score into the appropriate letter. The scaling of the score corresponds to the constant, the next level
-    (which means the next letter) is the next multiple of the constant. For example, if the constant is 38, D, the
-    4th letter, would correspond to the 4th multiple of 38... which is 152, so anything less than 152 is a D (or less).
+    (which means the next letter) is the next multiple of the constant. For example, if the constant is 39, D, the
+    4th letter, would correspond to the 4th multiple of 39... which is 156, so anything less than 156 is a D (or less).
 
     :param depth_of_material_score: the integer score of the depth of material
     :return: a character corresponding to the score.
     """
-    constant = 39
+    constant = complexity_constant()
     score_map = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
                  12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W',
                  23: 'X', 24: 'Y', 25: 'Z', 26: '-'}
