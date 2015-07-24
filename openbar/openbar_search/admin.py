0d92c913e9a0a4ac493ed702285f93d972a26120
Weston Novelli <wan1@rice.edu>
2015-07-24 14:15:29 -0500

    added boozecomplexityscore to admin panel

--- openbar/openbar_search/admin.py
+++ openbar/openbar_search/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin as django_admin
 from openbar_search import models
 
 # Register your models here.
-# django_admin.site.register(models.results_models.BasicComplexityScore)
+django_admin.site.register(models.BoozeComplexityScore)
 # django_admin.site.register(models.Preference)
